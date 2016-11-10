be423b986000e201902ac75ffab34589941bd188
Leo Famulari <leo@famulari.name>
2016-11-09 19:02:06 -0500

    Add a variant of OpenSSH that uses LibreSSL.
    * leo/packages/openssh.scm: New file.

---
+++ leo/packages/openssh.scm
@@ -0,0 +1,44 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2016 Leo Famulari <leo@famulari.name>
+;;;
+;;; This file is NOT part of GNU Guix, but is supposed to be used with GNU
+;;; Guix and thus has the same license.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (leo packages openssh)
+  #:use-module (guix packages)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix download)
+  #:use-module (guix build-system gnu)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages xorg)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages mit-krb5)
+  #:use-module (gnu packages groff)
+  #:use-module (gnu packages ssh)
+  #:use-module (gnu packages tls))
+
+;;; This is OpenSSH built against LibreSSL instead of OpenSSL.
+(define-public openssh-libressl
+  (package
+    (inherit openssh)
+    (name "openssh-libressl")
+    (inputs
+     `(("groff" ,groff)
+       ("libressl" ,libressl)
+       ("mit-krb5" ,mit-krb5)
+       ("pam" ,linux-pam)
+       ("xauth" ,xauth)
+       ("zlib" ,zlib)))))
