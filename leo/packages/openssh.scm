58cfd49cd1a58bce1bf7779b929399c57a7981a5
Leo Famulari <leo@famulari.name>
2017-03-19 22:27:43 -0400

    openssh-libressl: Use package-input-rewriting.
    * leo/packages/openssh.scm (libressl-instead-of-openssl): New procedure.
    (openssh-libressl): Use libressl-instead-of-openssl.
    [inputs]: Remove field.

--- leo/packages/openssh.scm
+++ leo/packages/openssh.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2016 Leo Famulari <leo@famulari.name>
+;;; Copyright © 2016, 2017 Leo Famulari <leo@famulari.name>
 ;;;
 ;;; This file is NOT part of GNU Guix, but is supposed to be used with GNU
 ;;; Guix and thus has the same license.
@@ -18,27 +18,15 @@
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (leo packages openssh)
-  #:use-module (guix packages)
-  #:use-module ((guix licenses) #:prefix license:)
-  #:use-module (guix download)
-  #:use-module (guix build-system gnu)
-  #:use-module (gnu packages compression)
-  #:use-module (gnu packages xorg)
-  #:use-module (gnu packages linux)
-  #:use-module (gnu packages kerberos)
-  #:use-module (gnu packages groff)
+  #:use-module (guix packages) ; package-input-rewriting
   #:use-module (gnu packages ssh)
   #:use-module (gnu packages tls))
 
-;;; This is OpenSSH built against LibreSSL instead of OpenSSL.
+(define libressl-instead-of-openssl
+  (package-input-rewriting `((,openssl . ,libressl))))
+
+;;; This is OpenSSH built with LibreSSL instead of OpenSSL.
 (define-public openssh-libressl
   (package
-    (inherit openssh)
-    (name "openssh-libressl")
-    (inputs
-     `(("groff" ,groff)
-       ("libressl" ,libressl)
-       ("mit-krb5" ,mit-krb5)
-       ("pam" ,linux-pam)
-       ("xauth" ,xauth)
-       ("zlib" ,zlib)))))
+    (inherit (libressl-instead-of-openssl openssh))
+    (name "openssh-libressl")))
